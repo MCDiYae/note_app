98a20a175008de561c0cf748c678ebdd2830b2d3
MCDiYae <mohammeddiyae.chbalou@etu.uae.ac.ma>
2023-01-02 13:12:05 +0100

    init hive & note box

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
-const primatyColor= Color.fromARGB(255, 57, 185, 205);
+const primatyColor = Color.fromARGB(255, 57, 185, 205);
+const noteBox = 'note_box';
