f063bdde3020f5c6b984b3efcd2f9cf991a94674
MCDiYae <mohammeddiyae.chbalou@etu.uae.ac.ma>
2023-01-11 13:40:05 +0100

    create notes state

---
+++ lib/cubits/notes cubit/notes_state.dart
@@ -0,0 +1,21 @@
+part of 'notes_cubit.dart';
+
+@immutable
+abstract class NotesState {}
+
+class NotesInitial extends NotesState {}
+
+class NotesLoading extends NotesState {}
+
+class NotesSucces extends NotesState {
+  final List<NoteModel> notes;
+
+  NotesSucces(this.notes);
+
+}
+
+class NotesFailure extends NotesState {
+  final String errMessage;
+
+  NotesFailure(this.errMessage);
+}
