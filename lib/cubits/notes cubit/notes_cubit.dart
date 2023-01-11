24b17f05a18793045fb92d6cdda7c7a92e11bc2c
MCDiYae <mohammeddiyae.chbalou@etu.uae.ac.ma>
2023-01-11 13:52:29 +0100

    create note cubit

--- lib/cubits/notes cubit/notes_cubit.dart
+++ lib/cubits/notes cubit/notes_cubit.dart
@@ -1,10 +1,25 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
 
+import '../../constant.dart';
 import '../../models/note_model.dart';
 
 part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
+  fetshAllNote() async {
+    emit(NotesLoading());
+    try {
+      var notesbox = Hive.box<NoteModel>(noteBox);
+      //c-a-d on stock data de type NoteModel
+      List<NoteModel> notes =
+          notesbox.values.toList(); // ici on obtient la liste des notes
+      emit(NotesSucces(notes));
+    } catch (e) {
+      emit(NotesFailure(e.toString()));
+    }
+  }
 }
+// et je vais ajouter ce cubit dans notes view
--- lib/view/note_view.dart
+++ lib/view/note_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:nnoteapp/cubits/notes%20cubit/notes_cubit.dart';
 import 'package:nnoteapp/view/widgets/bottom_add.dart';
 import 'package:nnoteapp/view/widgets/note_view_body.dart';
 
@@ -7,14 +9,17 @@ class NoteView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: const NoteViewBody(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-              context: context, builder: (context) => const AddBottom());
-        },
-        child: const Icon(Icons.add),
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+        body: const NoteViewBody(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet(
+                context: context, builder: (context) => const AddBottom());
+          },
+          child: const Icon(Icons.add),
+        ),
       ),
     );
   }
